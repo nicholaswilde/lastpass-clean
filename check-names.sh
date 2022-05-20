dbf8301375b6a2c261ff6b423e1d74d14a425b58
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-05-20 14:43:18 -0700

    Update check-names.sh

--- check-names.sh
+++ check-names.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# set -e
+set -o pipefail
 
+SCRIPT_VERSION="0.1.0"
+
+SCRIPT_NAME="$(basename "${0}")"
+SCRIPT_DESC="Check LastPass vault for dead urls"
+readonly SCRIPT_NAME
+readonly SCRIPT_DESC
+readonly SCRIPT_VERSION
+
+source ./lib/libbash
