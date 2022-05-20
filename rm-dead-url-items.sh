3d21c40ec647a5d7e6d9f0ce1945c84d4b38a79d
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-05-20 14:45:46 -0700

    Update rm-dead-url-items.sh

--- rm-dead-url-items.sh
+++ rm-dead-url-items.sh
@@ -5,7 +5,7 @@ set -o pipefail
 SCRIPT_VERSION="0.1.0"
 
 SCRIPT_NAME="$(basename "${0}")"
-SCRIPT_DESC="Check LastPass vault for dead urls"
+SCRIPT_DESC="Remove items with dead urls from LastPass vault"
 readonly SCRIPT_NAME
 readonly SCRIPT_DESC
 readonly SCRIPT_VERSION
